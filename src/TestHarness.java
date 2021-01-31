a9e1cffabd71f6c513289c8fc2a98a053916ca25
IlliaKucheruk200453638 <77804360+IlliaKucheruk200453638@users.noreply.github.com>
2021-01-31 15:39:56 -0500

    Initial commit

---
+++ src/TestHarness.java
@@ -0,0 +1,6 @@
+public class TestHarness {
+    public static void main(String[] args) {
+        Student obj = new Student("Illia", "Kucheruk", 200453638);
+        System.out.println(obj.getFirst_name()+" "+obj.getLast_name()+" "+obj.getStudent_number());
+    }
+}
